dea807c12da9e84832e5bb168012e400f1250371
unknown <mobinh264@gmail.com>
2021-08-22 01:28:51 +0600

    Functional Bank Varification code done

--- js/functional.js
+++ js/functional.js
@@ -1,18 +1,29 @@
 // input Field Grap Purpuss
 function inputField(fieldId){
-    const fieldInputId =  document.getElementById(fieldId);
-    const fieldInputValue = parseFloat(fieldInputId.value);
-    fieldInputId.value = '';
+    // const fieldInputId =  document.getElementById(fieldId);
+    // const fieldInputValue = parseFloat(fieldInputId.value);
+    const fieldInputValue = getInnerText(fieldId);
+    // fieldInputId.value = '';
+    document.getElementById(fieldId).value = '';
     return fieldInputValue;
 };
 
 // id Catch and amount update
 function valueUpdate( valueId , amount ){
-    const valueInputId = document.getElementById(valueId);
-    const valueInputNumber = parseFloat(valueInputId.innerText);
+    // const valueInputId = document.getElementById(valueId);
+    // const valueInputNumber = parseFloat(valueInputId.innerText);
+    const valueInputNumber = getInnerText(valueId);
     const updateValueAmount = valueInputNumber + amount;
-    valueInputId.innerText = updateValueAmount;
+    // valueInputId.innerText = updateValueAmount;
+    document.getElementById(valueId).innerText = updateValueAmount;
 };
+
+// get inner text Value
+function getInnerText(fieldId){
+    const totalTag = document.getElementById(fieldId);
+    const totalTagNum = parseFloat(totalTag.innerText);
+    return totalTagNum;
+}
 
 // calculation Total Amount
 function totalBalance( amount , isset ){
@@ -32,13 +43,18 @@ function totalBalance( amount , isset ){
 // deposit Button
 document.getElementById('deposit-button').addEventListener('click',function(){
    const amount =  inputField('deposit-input');
-    valueUpdate( 'deposit-total', amount );
-    totalBalance( amount, true );
+    if( amount > 0 ){
+        valueUpdate( 'deposit-total', amount );
+        totalBalance( amount, true );
+    }
 });
 
 // withDraw Button
 document.getElementById('withdraw-button').addEventListener('click',function(){
     const amount = inputField('withdraw-input');
-     valueUpdate('withdraw-total', amount);
-     totalBalance( amount, false );
+    const totalTag = getInnerText('balance-total');
+     if( amount > 0 && amount <= totalTag ){
+        valueUpdate('withdraw-total', amount);
+        totalBalance( amount, false );
+     }
 });
